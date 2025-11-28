974ef9a1ca0dc149e40b4e117b4b28fe79b0b252
Percy <percymichael0@gmail.com>
2025-11-28 16:01:00 +0300

    yeah man

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,13 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
+Route::get('/max-upload', function () {
+    return response()->json([
+        'upload_max_filesize' => ini_get('upload_max_filesize'),
+        'post_max_size' => ini_get('post_max_size'),
+    ]);
+})->name('max-upload');
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
